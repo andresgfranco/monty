b591d4f44440eed8f8e9e9e6fed7258e6ac19a55
andresgfranco <andresgfranco0@gmail.com>
2020-12-21 22:55:36 -0500

    Added a new line

--- monty.c
+++ monty.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 
 	if (OPFile == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file %s", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while ((read = getline(&line, &len, OPFile)) != -1)

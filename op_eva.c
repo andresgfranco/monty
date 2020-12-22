5626151e5b9da511a1590e7479b6b5c87b3ae6e7
Roberth <rrondoc@gmail.com>
2020-12-21 23:45:45 -0500

    uploading task2

--- op_eva.c
+++ op_eva.c
@@ -9,11 +9,11 @@
 void op_eva(char *given_opcode, stack_t **stack, unsigned int line_number)
 {
 	instruction_t opcode_func[] = {{"push", op_push}, {"pall", op_pall},
-		{"pint", op_pint}, {NULL, NULL}};
+		{"pint", op_pint}, {"pop", op_pop} {NULL, NULL}};
 
 	int counter = 0;
 
-	while (counter < 3)
+	while (counter < 4)
 	{
 		if (strcmp(opcode_func[counter].opcode, given_opcode) == 0)
 		{

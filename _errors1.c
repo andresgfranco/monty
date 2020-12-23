0f7e4e36dd2d070c1f2ca5b40f26e66d907e2643
andresgfranco <andresgfranco0@gmail.com>
2020-12-22 23:20:50 -0500

    Adding function pchar

--- _errors1.c
+++ _errors1.c
@@ -22,5 +22,14 @@ void printerror1(int error, unsigned int line_number)
 		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
+	if (error == 12)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (error == 13)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 }
--- opcodes_3.c
+++ opcodes_3.c
@@ -39,3 +39,26 @@ void op_hashtag(stack_t **stack, unsigned int line_number)
 	(void)line_number;
 	(void)**stack;
 }
+/**
+ * op_pchar - prints the char at the top of the stack,
+ * followed by a new line.
+ * @stack: doubly linked list working as a stack
+ * @line_number: line number of the specified opcode
+ * Return: void
+**/
+void op_pchar(stack_t **stack, unsigned int line_number)
+{
+	int character = (*stack)->n;
+
+	if ((*stack) == NULL)
+	{
+		free_stack(*stack);
+		printerror(13, line_number);
+	}
+	if (character < 0 && character > 127)
+	{
+		free_stack(*stack);
+		printerror(12, line_number);
+	}
+	printf("%c\n", (*stack)->n);
+}
